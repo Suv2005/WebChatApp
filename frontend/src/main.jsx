41de6d5d4dea2cbee0cd5c0909f39b273402ce79
Suv2005 <munmun79debnath@gmail.com>
2026-07-03 01:59:44 +0530

    refactor: remove publishable key check from ClerkProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
 
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Clerk Publishable Key");
-}
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <ClerkProvider>
       <App />
     </ClerkProvider>
   </StrictMode>,
